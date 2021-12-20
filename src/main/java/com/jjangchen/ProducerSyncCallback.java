f4caf67e9c40992d074f9286893a6940acb65ab6
Lee Ji HO <dlwlgh9412@gmail.com>
2021-12-20 15:51:44 +0900

    브로커 정상여부 확인 프로듀서

--- src/main/java/com/jjangchen/ProducerSyncCallback.java
+++ src/main/java/com/jjangchen/ProducerSyncCallback.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class ProducerSyncCallback {
+package com.jjangchen;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+// 동기로 프로듀서의 전송 결과를 확인하는 것은 브로커로부터 응답 값을 받기전 까지 대기해야하기 때문에 속도 저하가 있음
+public class ProducerSyncCallback {
+    private final static Logger logger = LoggerFactory.getLogger(ProducerSyncCallback.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        // 직렬화 설정
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "jjang", "23");
+        try {
+            RecordMetadata metadata = producer.send(record).get();
+            logger.info(metadata.toString());
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        } finally {
+            producer.flush();
+            producer.close();
+        }
+    }
 }
